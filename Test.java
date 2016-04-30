5dbc32434f71e6e084e37cb740f2afb925f963c2
Marek Bodziony <marek_bodziony@o2.pl>
2016-04-30 06:44:47 +0200

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+package document;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		BasicDocument doc = new BasicDocument ("Have me here. Are e you all ok?!"); //Ala me kota. Kot ma Ale!! O godz33 9 bedziemy? si3,byc");
+		
+		System.out.println("\n"+ doc.getNumSyllables());
+
+	}
+
+}
